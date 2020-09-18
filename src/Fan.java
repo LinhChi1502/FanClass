734f0dff2e32c6b9bdb2363c135e07c81cd2f85f
LinhChi1502 <linhchidkc@gmail.com>
2020-09-18 20:41:37 +0700

    create file

---
+++ src/Fan.java
@@ -0,0 +1,65 @@
+public class Fan {
+    static final int SLOW = 1;
+    static final int MEDIUM = 2;
+    static final int FAST = 3;
+    private int speed = SLOW;
+    private boolean isOn = false;
+    private double radius = 5;
+    private String color = "blue";
+
+    public Fan() {
+
+    }
+
+    public static int getSLOW() {
+        return SLOW;
+    }
+
+    public static int getMEDIUM() {
+        return MEDIUM;
+    }
+
+    public static int getFAST() {
+        return FAST;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public boolean isOn() {
+        return isOn;
+    }
+
+    public void setOn(boolean on) {
+        isOn = on;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String toString() {
+        if (this.isOn) {
+            return this.speed + " " + this.color + " " + this.radius + " fan is on";
+        } else {
+            return this.color + " " + this.radius + " fan is off";
+        }
+    }
+}
